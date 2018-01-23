8b71215929badee7eabefc5956c93ad20722ff99
Valeria <valeriaweb@gmail.com>
2018-01-23 15:50:22 -0200

    criando controllers

--- controllers/usuarios.go
+++ controllers/usuarios.go
@@ -1,1 +1,13 @@
 package controllers
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+//Home é a página inicial da minha aplicação
+func Home(c echo.Context) error {
+
+	return c.String(http.StatusOK, "Hello, World! GO GO")
+}

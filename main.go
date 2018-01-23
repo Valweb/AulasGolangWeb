36d236a9a00fb0dc9d8fef69867fb86aed4f0847
Valeria <valeriaweb@gmail.com>
2018-01-23 15:39:02 -0200

    Configurando middleware

--- main.go
+++ main.go
@@ -2,25 +2,14 @@ package main
 
 import (
 	r "github.com/Valweb/AulasGolangWeb/routers"
-	_ "github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	_ "github.com/labstack/echo/middleware"
 )
 
-/*
-Primeira forma de fazer  um callback
+//Primeira forma de fazer  um callback
 func main() {
-	e := echo.New()
-	//criar uma rota para fazer uma requisição para o provedor
-	e.GET("/", func(c echo.Context) error {
-		//varial c será tipo context
-		return c.String(http.StatusOK, "Hello, World!")
+	e := r.App
 
-	})
-	//Número da porta que o Go ficará escutando
-	e.Start(":3000")
-}
-*/
-
-//Segunda forma de fazer um callback
-func main() {
-	r.App.Start(":3000")
+	e.Use(middleware.Logger()) //Cada requisição ele estará exibindo no formato de log no terminal.
+	e.Logger.Fatal(e.Start(":3000"))
 }

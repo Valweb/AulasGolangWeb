e471dcf9d2e535bdcef34d387abeb16caa703ebc
Valeria <valeriaweb@gmail.com>
2018-01-23 16:24:02 -0200

    Criando camada models

--- models/usuarios.go
+++ models/usuarios.go
@@ -1,1 +1,8 @@
 package models
+
+//Usuarios representa a tabela usuários na base de dados. Struct sempre iniciada com letra maiúscula. O campo é representado por letras minúsculas no banco de dados e no json
+type Usuarios struct {
+	ID    int    `db: "id" json:"id"`
+	Nome  string `db:"nome" json"nome"`
+	Email string `db:"email" json"email"`
+}
